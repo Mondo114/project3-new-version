711cda25282355f6f7af0998efcb3fc47e441aad
Daniel Silber <dsilber@Daniels-MacBook-Pro.local>
2018-04-25 11:42:24 -0700

    NewWeather

--- client/src/components/Weather/Weather.js
+++ client/src/components/Weather/Weather.js
@@ -1,35 +1,29 @@
 
 import React from 'react';
 import axios from 'axios';
-import './Weather.css';
-import Forecast from 'react-forecast';
-import React from "react";
-import "./Weather.css";
 
-const Weather = () => (
-  <weather className="weather">
-    <span>Weather</span>
-  </weather>
-);
 
+
+var request = require('axios');
+
+
+const API_URL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}"
 const API_KEY = "001ab0b7efc2baeae3d7cdf13d00c7e8";
 
-class weather extends React.Component {
-  getWeather = async (e) => {
-    e.preventDefault();
-    const api_call = await fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}");
-    //const data = await api_call.json();
-    console.log(api_call);
-  }
-  render() {
-    return (
-      <weather className="weather">
-      <span>Weather</span>
-    </weather>
-    );
+ class getWeather extends React.Component {
+  state = {
+    weather: []
   }
 
-};  
+  componentDidMount() {
+    axios.get("api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}")
+    .then(res => {
+      const weather = res.data;
+      this.setState({ weather });
+      console.log(weather);
+    })
+  }
+}
 
 
 const Weather = () => (
